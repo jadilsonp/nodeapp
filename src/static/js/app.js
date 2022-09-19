b6e6aa4a750682da335feec96b0df8e006627668
Jadilson <jadilsonpaiva@hotmail.com>
2022-09-19 18:50:10 +0000

    teste

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">COLOQUE ITENS AQUI!</p>
+                <p className="text-center">ME ATUALIZE AQUI!!!!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
